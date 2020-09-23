b7b49868ce8bd30548d2c619736ac20a7c2054ed
unknown <cpbram2011@gmail.com>
2020-09-23 13:04:53 -0500

    Sass Setup

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -7,18 +7,22 @@ import WelcomeContainer from './welcome/welcome_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 
+
 export default () => (
     <div>
         <header id='welcome-bar'>
             <Link to='/' id='contrafact'><h1>Contrafact</h1></Link>
+
             <div >
                 <WelcomeContainer />
             </div>
 
         </header>
+
         <Switch>
             <AuthRoute exact path="/login" component={LogInFormContainter} />
             <AuthRoute exact path="/signup" component={SignUpFormContainter} />
+            
         </Switch>
     </div>
 )
