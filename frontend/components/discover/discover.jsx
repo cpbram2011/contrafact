cb118e73d75de06aa3746ed5bfb683ed9241c656
Chris Bram <cpbram2011@gmail.com>
2020-09-23 14:59:20 -0500

    Minor Tweaks

---
+++ frontend/components/discover/discover.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+export default () => (
+    <div id='discover'>
+        <h1>List of Songs</h1>
+        <ul>
+            <li>Friday Nights</li>
+            <li>Saturday Nights</li>
+            <li>Sunday Nights</li>
+            <li>Tuesday Nights</li>
+        </ul>
+    </div>
+
+
+);
+
---
+++ frontend/components/splash/splash.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => (
+
+
+    <div id="splash">
+        <h1>Never stop discovering</h1>
+        <p>Save your favorite songs, build and follow playlists. All for free.</p>
+        
+    </div>
+)   
