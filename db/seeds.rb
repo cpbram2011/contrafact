9c1b170d4c7511efd7d24e3776de968bbbffaea2
unknown <cpbram2011@gmail.com>
2020-09-27 19:42:26 -0500

    Add to Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,15 +22,34 @@ users = 4.times.map {|i| User.create!({
 
 s1 = Song.create(title: 'Afro Blue', artist: 'Robert Glasper')
 s1_cover = open("https://contrafact-seeds.s3.us-east-2.amazonaws.com/BlackRadio.jpg")
+s1_track_url = open("https://contrafact-seeds.s3.us-east-2.amazonaws.com/Robert+Glasper+Experiment+-+Afro+Blue+(Feat.+Erykah+Badu)+(128+kbps)+(1).mp3")
 s1.cover.attach(io: s1_cover, filename: "s1-cover.jpg")
-track_url = open("https://contrafact-seeds.s3.us-east-2.amazonaws.com/Robert+Glasper+Experiment+-+Afro+Blue+(Feat.+Erykah+Badu)+(128+kbps)+(1).mp3")
-s1.track.attach(io: track_url, filename: 's1.mp3')
+s1.track.attach(io: s1_track_url, filename: 's1.mp3')
 
 s2 = Song.create(title: 'Sniff', artist: 'DOMi & JD Beck')
 s2_cover = open("https://contrafact-seeds.s3.us-east-2.amazonaws.com/domijd.jpg")
+s2_track_url = open("https://contrafact-seeds.s3.us-east-2.amazonaws.com/Nord+Live+Sessions+-+DOMi+%26+JD+Beck+-+Sniff.mp3")
 s2.cover.attach(io: s2_cover, filename: "s2-cover.jpg")
-track_url = open("https://contrafact-seeds.s3.us-east-2.amazonaws.com/Nord+Live+Sessions+-+DOMi+%26+JD+Beck+-+Sniff.mp3")
-s2.track.attach(io: track_url, filename: 's2.mp3')
+s2.track.attach(io: s2_track_url, filename: 's2.mp3')
+
+
+s3 = Song.create(title: 'Desire (Live \'15)', artist: 'Hiromi Trio Project')
+s3_cover = open("https://contrafact-seeds.s3.us-east-2.amazonaws.com/voice.jpeg.jpg")
+s3_track_url = open("https://contrafact-seeds.s3.us-east-2.amazonaws.com/Hiromi+Uehara+The+Trio+Project+Desire+(128+kbps).mp3")
+s3.cover.attach(io: s3_cover, filename: "s3-cover.jpg")
+s3.track.attach(io: s3_track_url, filename: 's3.mp3')
+
+
+s4 = Song.create(title: 'Lingus', artist: 'Snarky Puppy')
+s4_cover = open("https://contrafact-seeds.s3.us-east-2.amazonaws.com/welikeithere.jpg")
+s4_track_url = open("https://contrafact-seeds.s3.us-east-2.amazonaws.com/Snarky+Puppy+-+Lingus+(We+Like+It+Here)+(128+kbps).mp3")
+s4.cover.attach(io: s4_cover, filename: "s4-cover.jpg")
+s4.track.attach(io: s4_track_url, filename: 's4.mp3')
+
+
+
+
+
 
 
 
@@ -40,14 +59,3 @@ s2.track.attach(io: track_url, filename: 's2.mp3')
 # s2.cover.attach(io: s2_cover, filename: "s2-cover.jpg")
 # track_url = open("")
 # s2.track.attach(io: track_url, filename: 's2.mp3')
-
-
-# song2 = Song.create(title: 'Afro Red', artist: 'Robert Glasper')
-# song3 = Song.create(title: 'Nardis', artist: 'Bill Evans')
-# song4 = Song.create(title: 'Afro Green', artist: 'Robert Glasper')
-# song5 = Song.create(title: 'Afro Yellow', artist: 'Robert Glasper')
-# song6 = Song.create(title: 'Afro Black', artist: 'Robert Glasper')
-
-
-
-
